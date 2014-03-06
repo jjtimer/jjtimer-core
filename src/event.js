513bdb247890cffa7b69ca16a2ce7d74393aaed9
Joey Gouly <joey.gouly@gmail.com>
2014-03-06 21:38:31 +0000

    Basic implementation of the pub/sub pattern in event.js

--- src/event.js
+++ src/event.js
@@ -1,5 +1,27 @@
 var Event = (function() {
   'use strict';
 
-  return { };
+  var events = [];
+
+  function on(event, fn) {
+    if (!events[event]) {
+      events[event] = [];
+    }
+    events[event].push(fn);
+  }
+
+  function emit(event, args) {
+    var fns = events[event];
+    if (!fns)
+      return;
+
+    fns.forEach(function(fn, idx, arry) {
+      fn(args);
+    });
+  }
+
+  return {
+    on: on,
+    emit: emit
+  };
 });

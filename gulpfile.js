a14682c34fb482ee083af5515b2a3a4eaeda3e85
Joey Gouly <joey.gouly@gmail.com>
2014-03-27 22:30:20 +0000

    Add another gulp task 'test'.
    Unsurprisingly this just calls 'mocha'.

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,14 @@ var gulp = require('gulp');
 
 var jshint = require('gulp-jshint');
 
+var mocha = require('gulp-mocha');
+
 gulp.task('lint', function() {
   return gulp.src('src/*.js').
     pipe(jshint()).
     pipe(jshint.reporter('default'));
+});
+
+gulp.task('test', function() {
+  return gulp.src('test/*.js').pipe(mocha());
 });
